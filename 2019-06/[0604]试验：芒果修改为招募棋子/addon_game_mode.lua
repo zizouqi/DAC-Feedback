c2a5c4326590cda01c3726611a4fbe8b7c262f04
Anonymous <a@gmail.com>
2019-06-04 13:14:41 +0800

    [0604]试验：芒果修改为招募棋子

--- 2019-06/[0604]试验：芒果修改为招募棋子/addon_game_mode.lua
+++ 2019-06/[0604]试验：芒果修改为招募棋子/addon_game_mode.lua
@@ -1,6 +1,7 @@
 function ItemMangguo(keys)
 	local caster = keys.caster
-	AddMana(caster, RandomInt(1,5))
+	--AddMana(caster, RandomInt(1,5))
+	Draw5ChessAndShow(caster.at_team_id or caster.team_id, false)
 	EmitSoundOn("DOTA_Item.Mango.Activate",caster)
 	play_particle('particles/items3_fx/mango_active.vpcf',PATTACH_ABSORIGIN_FOLLOW,caster,3)
 end
