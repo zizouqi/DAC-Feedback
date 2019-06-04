6734f859b14fd5f5f22610da6ba1a26cd71d7d5b
Anonymous <a@gmail.com>
2019-06-04 16:14:59 +0800

    [0604]试验：芒果增加金币根据回合数变化

--- 2019-06/[0604]试验：芒果增加金币根据回合数变化/addon_game_mode.lua
+++ 2019-06/[0604]试验：芒果增加金币根据回合数变化/addon_game_mode.lua
@@ -1,6 +1,6 @@
 function ItemMangguo(keys)
 	local caster = keys.caster
-	AddMana(caster, RandomInt(1,5))
+	AddMana(caster, RandomInt(1,3) + math.floor((GameRules:GetGameModeEntity().battle_round-1)/12))
 	EmitSoundOn("DOTA_Item.Mango.Activate",caster)
 	play_particle('particles/items3_fx/mango_active.vpcf',PATTACH_ABSORIGIN_FOLLOW,caster,3)
 end
