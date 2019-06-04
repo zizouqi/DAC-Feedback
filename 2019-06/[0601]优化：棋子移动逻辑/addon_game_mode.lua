d3fcd2740f85734416d19e3edbb2b9019d56b5cd
Anonymous <a@gmail.com>
2019-06-04 13:48:07 +0800

    [0601]优化：棋子移动逻辑

--- 2019-06/[0601]优化：棋子移动逻辑/addon_game_mode.lua
+++ 2019-06/[0601]优化：棋子移动逻辑/addon_game_mode.lua
@@ -1,7 +1,10 @@
 function FindNextSkipPosition(u)
 	local team_id = u.at_team_id or u.team_id
 	local skip_postion = nil
+	local skip_postion_alt = nil
+	local skip_postion_new = nil
 	local length2d = 99999
+	local length2d_alt = 99999
 	local pos1 = XY2Vector(u.x,u.y,team_id)
 	for x=1,8 do
 		for y=1,8 do
@@ -12,8 +15,18 @@ function FindNextSkipPosition(u)
 					skip_postion = next_skip
 					length2d = (pos2-pos1):Length2D()
 				end
+				if next_skip ~= nil and (pos2-pos1):Length2D() < length2d_alt and math.abs(u.x-x) == math.abs(u.y-y) and pos2 == IsGridCanReach(x,y,u) then
+					skip_postion_alt = next_skip
+					length2d_alt = (pos2-pos1):Length2D()
+				end
 			end
 		end
 	end
-	return skip_postion
+	
+	if skip_postion_alt ~= nil and (skip_postion_alt-skip_postion):Length2D() < 200 and (skip_postion_alt-pos1):Length2D() > 200 then
+		skip_postion_new = skip_postion_alt
+	else
+		skip_postion_new = skip_postion
+	end
+	return skip_postion_new
 end
