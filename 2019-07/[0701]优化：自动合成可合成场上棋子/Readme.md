4720b857c74038db889b81025f2d353cc0ec6ad2
Anonymous <a@gmail.com>
2019-07-03 22:53:49 +0800

    Revert "[0701]优化：自动合成可合成场上棋子 Modified"
    This reverts commit aff6c01adafc86a1e1f99ac6c565dfc0c47c4087.

--- 2019-07/[0701]优化：自动合成可合成场上棋子/Readme.md
+++
@@ -1,5 +0,0 @@
-## 优化：自动合成可合成场上棋子
-
-类似于《Underlords》的操作，效果如图。
-
-<img src="https://github.com/zizouqi/DAC-Feedback/blob/master/Misc/Image/201907/combine.gif" alt="after" title="after" />
--- 2019-07/[0701]优化：自动合成可合成场上棋子/addon_game_mode.lua
+++ 2019-07/[0701]优化：自动合成可合成场上棋子/addon_game_mode.lua
@@ -2,21 +2,16 @@ function DAC:OnRequestBuyChess(keys)
 
 	--（略）
 
-	--判断是否有两个一样的棋子，有的话直接合成
-	local have_exist_count,chess1,chess2,chess3 = Find2SameChessInHandOrOnBoard(h,chess)
+	--判断手牌里是否有两个一样的，有的话直接合成
+	local have_exist_count,chess1,chess2,chess3 = Find2SameChessInHand(h,chess)
 
 	--（略）
 
-	--直接合成一个2/3星的 或者 添加一个1星的
-	local have_exist_count,chess1,chess2 = Find2SameChessInHandOrOnBoard(h,chess)
+	--直接合成一个2星的 或者 添加一个1星的
+	local have_exist_count,chess1,chess2 = Find2SameChessInHand(h,chess)
 	if have_exist_count >= 2 and chess1 ~= nil and chess2 ~= nil and h.is_auto_combine == 1 then
-		local advanve_chess = chess..'1'
-		local advanve_have_exist_count,advanve_chess1,advanve_chess2 = Find2SameChessInHandOrOnBoard(h,advanve_chess)
-		if advanve_have_exist_count >= 2 then
-			CombineChessPlus({[1] = chess1,[2] = chess2,[3] = advanve_chess1,[4] = advanve_chess2},(chess..'11'))
-		else
-			CombineChessPlus({[1] = chess1,[2] = chess2},(chess..'1'))
-		end
+
+		CombineChessPlus({[1] = chess1,[2] = chess2},(chess..'1'))
 
 		-- local items_table = GetAllItemsInUnits({
 		-- 	[1] = chess1,
@@ -58,53 +53,8 @@ function CombineChessPlus(units, advance_unit_name)
 	for _,u in pairs(units) do
 		if u.hand_index ~= nil then
 			GameRules:GetGameModeEntity().hand[team_id][u.hand_index] = 0
-			hero.hand_entities[u.hand_index] = nil
+			hero.hand_entities[hand_index] = nil
 
 	--（略）
 
-end
-
-
-function Find2SameChessInHandOrOnBoard(caster,chess)
-	if chess == 'chess_io1' or chess == 'chess_io' then
-		--2星小精灵不参与合成
-		return 0,nil,nil,nil
-	end
-	local count = 0
-	local chess1 = nil
-	local chess2 = nil
-	local chess3 = nil
-	if caster ~= nil and caster.hand_entities ~= nil then  -- and GameRules:GetGameModeEntity().is_game_test == false
-		for _,v in pairs(GameRules:GetGameModeEntity().to_be_destory_list[caster.team_id]) do
-			if IsUnitExist(v) == true and v:GetUnitName() == chess and v:FindAbilityByName('is_druid') == nil then
-				count = count + 1
-				if count == 1 then
-					chess1 = v
-				end
-				if count == 2 then
-					chess2 = v
-				end
-				if count == 3 then
-					chess3 = v
-				end
-			end
-		end
-		for _,v in pairs(caster.hand_entities) do
-			if IsUnitExist(v) == true and v:GetUnitName() == chess and v:FindAbilityByName('is_druid') == nil then
-				count = count + 1
-				if count == 1 then
-					chess1 = v
-				end
-				if count == 2 then
-					chess2 = v
-				end
-				if count == 3 then
-					chess3 = v
-				end
-			end
-		end
-		return count,chess1,chess2,chess3
-	else
-		return 0,nil,nil,nil
-	end
 end
