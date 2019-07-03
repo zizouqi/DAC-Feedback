287eb93e30360ded6df615a445c7685d67eb6a35
Anonymous <a@gmail.com>
2019-07-03 22:57:44 +0800

    [0703]实验：抽卡时高亮显示已拥有的一星棋子

---
+++ 2019-07/[0703]实验：抽卡时高亮显示已拥有的一星棋子/dac.js
@@ -0,0 +1,41 @@
+function OnShowDrawCard(keys){
+    if (!CheckClientKey(keys.key)) return;
+    if (keys.unlock == true){
+        Game.EmitSound("ui.crafting_newslot");
+        IS_PANEL_DRAW_CARD_LOCKED = false;
+        $("#image_lock").SetImage("file://{images}/custom_game/unlock.png");
+        $("#image_lock").style['opacity'] = '0.1';
+    }
+    if (keys.auto_unlock == true){
+        Game.EmitSound("ui.crafting_newslot");
+        IS_PANEL_DRAW_CARD_LOCKED = false;
+        $("#image_lock").SetImage("file://{images}/custom_game/unlock.png");
+        $("#image_lock").style['opacity'] = '0.1';
+        return;
+    }
+    var cards = keys.cards.split(',');
+    MY_DRAW_CHESS_LIST = {};
+    if (cards && cards.length>1){
+        for (var i=0;i<cards.length;i++){
+            if (cards[i]){
+                MY_DRAW_CHESS_LIST[i] = cards[i];
+            }
+        }
+    }
+    show_panel_draw_card();
+    GameUI.SelectUnit( -1, false );
+}
+function set_draw_card_status(){
+    if (MY_DRAW_CHESS_LIST){
+        for (var i in MY_DRAW_CHESS_LIST){
+            var price = CHESS_2_LEVEL[MY_DRAW_CHESS_LIST[i]];
+            var my_gold = MY_GOLD || Math.round(Entities.GetMana(Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer())));
+            if (price > my_gold){
+                $('#text_draw_card_price_'+i).style['color'] = '#ff0000';
+            }
+            else{
+                $('#text_draw_card_price_'+i).style['color'] = '#ffffff';
+            }
+        }
+    }
+}
