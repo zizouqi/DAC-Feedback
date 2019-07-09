568586f0e4b8802ae3f3317c1bb358c382846cb3
Anonymous <a@gmail.com>
2019-07-09 15:11:23 +0800

    Revert "[0709]优化：水人沙王根据技能有效范围寻找最远格子 Modified"
    This reverts commit 2200b1e1fe4efe040fca725a39286bc3b40ed72c.

--- 2019-07/[0709]优化：水人沙王根据技能有效范围寻找最远格子/Readme.md
+++
@@ -1,7 +0,0 @@
-## 优化：水人沙王根据技能有效范围寻找最远格子
-
-目前水人施法是选择目标位置的逻辑是最远且能攻击到敌人的格子。但因为水人是远程单位，故在一些情况下会有技能路径上没有敌方单位导致空放技能的情况。
-
-优化的代码则是根据位移技能的有效伤害范围（npc_abilities_custom.txt 内对技能定义的 width 值）来选取最远的目标格子。
-
-<img src="https://github.com/zizouqi/DAC-Feedback/blob/master/Misc/Image/201907/morph.gif" alt="after" title="after" />
--- 2019-07/[0709]优化：水人沙王根据技能有效范围寻找最远格子/addon_game_mode.lua
+++ 2019-07/[0709]优化：水人沙王根据技能有效范围寻找最远格子/addon_game_mode.lua
@@ -2,52 +2,7 @@ function ChessAI(u)
 	-- (略)
 	elseif GameRules:GetGameModeEntity().ability_behavior_list[a] == 11 then
 							
-		local unluckypoint = FindFarthestGridForAbility(u,a)
+		local unluckypoint = FindFarthestCanAttackEnemyEmptyGrid(u)
 	
 	-- (略)
-end
-
---水人或沙王用：寻找一个最远的并且在技能伤害范围内的格子，
-function FindFarthestGridForAbility(u,a)
-	local team_id = u.at_team_id or u.team_id
-	local length2d = 0
-	local pos1 = u:GetAbsOrigin()
-	local skip_postion = nil
-	local range = 80
-
-	for k,v in pairs(u:FindAbilityByName(a):GetAbilityKeyValues()) do
-        if k == "AbilitySpecial" then
-            for l,m in pairs(v) do
-                if m['width'] then
-                    range = m['width']
-                end
-            end
-        end
-    end
-
-	for x=1,8 do
-		for y=1,8 do
-			local pos2 = XY2Vector(x,y,team_id)
-			if IsEmptyGrid(team_id,x,y) and IsGridCanAttackEnemyInRange(x,y,u,range) == true then
-				if (pos2-pos1):Length2D() > length2d then
-					skip_postion = pos2
-					length2d = (pos2-pos1):Length2D()
-				end
-			end
-		end
-	end
-
-	return skip_postion
-end
-
---判断格子是否在范围内
-function IsGridCanAttackEnemyInRange(x,y,u,range)
-	local team_id = u.at_team_id or u.team_id
-	--遍历所有单位
-	for _,enemy in pairs (GameRules:GetGameModeEntity().to_be_destory_list[team_id]) do
-		if enemy.team_id ~= u.team_id and enemy:IsInvisible() == false and (XY2Vector(x,y,team_id) - enemy:GetAbsOrigin()):Length2D() < range + enemy:GetHullRadius() + u:GetHullRadius() and enemy:HasModifier('modifier_winter_wyvern_cold_embrace') == false and IsBozangWudi(enemy) == false then
-			return true
-		end
-	end
-	return false
 end
