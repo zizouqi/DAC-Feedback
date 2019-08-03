a7756f2bae5ab34ebdc70ce61a97d8267d5ca121
Anonymous <a@gmail.com>
2019-08-03 17:21:03 +0800

    [0803]优化：小精灵优先选择高费德鲁伊合成 Modified

--- 2019-08/[0803]优化：小精灵优先选择高费德鲁伊合成/addon_game_mode.lua
+++ 2019-08/[0803]优化：小精灵优先选择高费德鲁伊合成/addon_game_mode.lua
@@ -1,13 +1,15 @@
 --为小精灵找一个指定level星级的德鲁伊棋子
 function Find1ChessByIO(team_id,level)
+	local cost = 0
+	local combine_chess = nil
 	for uuu,v1 in pairs(GameRules:GetGameModeEntity().mychess[team_id]) do
-		if v1 ~= nil and v1.combining ~= true then
-			if level == 1 and (v1.chess == 'chess_eh' or v1.chess == 'chess_fur' or v1.chess == 'chess_tp' or v1.chess == 'chess_ld') then
-				return v1
-			end
-			if level == 2 and (v1.chess == 'chess_eh1' or v1.chess == 'chess_fur1' or v1.chess == 'chess_tp1' or v1.chess == 'chess_ld1') then
-				return v1
+		if v1 ~= nil and v1.combining ~= true and EntIndexToHScript(v1.index):FindAbilityByName('is_druid') ~= nil then
+			local chess = EntIndexToHScript(v1.index)
+			if GetChessStar(chess) == level and cost < GameRules:GetGameModeEntity().chess_2_mana[GetChessTypeName(chess)] then
+				cost = GameRules:GetGameModeEntity().chess_2_mana[GetChessTypeName(chess)]
+				combine_chess = v1
 			end
 		end
 	end
+	return combine_chess
 end
