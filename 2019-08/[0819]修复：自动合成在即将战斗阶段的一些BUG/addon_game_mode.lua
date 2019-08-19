cfa1b33d3317cb800f00829b07576ccca7fd9395
Anonymous <a@gmail.com>
2019-08-19 13:19:59 +0800

    [0819]修复：自动合成在即将战斗阶段的一些BUG Modified

--- 2019-08/[0819]修复：自动合成在即将战斗阶段的一些BUG/addon_game_mode.lua
+++ 2019-08/[0819]修复：自动合成在即将战斗阶段的一些BUG/addon_game_mode.lua
@@ -8,18 +8,20 @@ function Find2SameChessInHandOrOnBoard(caster,chess)
 	local chess2 = nil
 	local chess3 = nil
 	if caster ~= nil and caster.hand_entities ~= nil then  -- and GameRules:GetGameModeEntity().is_game_test == false
-		for _,v in pairs(GameRules:GetGameModeEntity().to_be_destory_list[caster.team_id]) do
-			prt(v.team_id)
-			if IsUnitExist(v) == true and v:GetUnitName() == chess and (v:FindAbilityByName('is_druid') == nil or (v:FindAbilityByName('is_druid') ~= nil and GetChessStar(v) == 2)) and v.is_in_battle ~= true and v.is_moving ~= true then
-				count = count + 1
-				if count == 1 then
-					chess1 = v
-				end
-				if count == 2 then
-					chess2 = v
-				end
-				if count == 3 then
-					chess3 = v
+		if GameRules:GetGameModeEntity().prepare_timer > 5 then  -- 准备回合结束后，不再寻找场上棋子做为合成材料，避免一些未知的BUG。
+			for _,v in pairs(GameRules:GetGameModeEntity().to_be_destory_list[caster.team_id]) do
+				prt(v.team_id)
+				if IsUnitExist(v) == true and v:GetUnitName() == chess and (v:FindAbilityByName('is_druid') == nil or (v:FindAbilityByName('is_druid') ~= nil and GetChessStar(v) == 2)) and v.is_in_battle ~= true and v.is_moving ~= true and v.team_id == caster.team_id then
+					count = count + 1
+					if count == 1 then
+						chess1 = v
+					end
+					if count == 2 then
+						chess2 = v
+					end
+					if count == 3 then
+						chess3 = v
+					end
 				end
 			end
 		end
