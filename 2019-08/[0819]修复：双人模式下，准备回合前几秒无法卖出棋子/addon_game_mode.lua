7fa7066dc8d4a2b6203b75dfabd2a1b6f3887856
Anonymous <a@gmail.com>
2019-08-19 12:53:10 +0800

    [0819]修复：双人模式下，准备回合前几秒无法卖出棋子 Modified

--- 2019-08/[0819]修复：双人模式下，准备回合前几秒无法卖出棋子/addon_game_mode.lua
+++ 2019-08/[0819]修复：双人模式下，准备回合前几秒无法卖出棋子/addon_game_mode.lua
@@ -5,6 +5,10 @@ function RemoveChess(keys)
 	local team_id = target.team_id
 	local x = Vector2X(position,team_id)
 	local y = Vector2Y(position,team_id)
+	local extra_time = 0
+	if GetMapName() == 'casual_2x4_ob' or GetMapName() == 'casual_2x4' or GetMapName() == 'ranked_2x4' then
+		extra_time = 5
+	end
 
 	if string.find(target:GetUnitName(),'chess_') == nil then
 		return
@@ -14,6 +18,6 @@ function RemoveChess(keys)
 		return
 	end	
 	if keys.force_remove ~= true then
-		if (GameRules:GetGameModeEntity().game_status == 2 and target.hand_index == nil) or (GameRules:GetGameModeEntity().game_status == 1 and target.hand_index == nil and (GameRules:GetGameModeEntity().prepare_timer > 33 or GameRules:GetGameModeEntity().prepare_timer < 2)) then
+		if (GameRules:GetGameModeEntity().game_status == 2 and target.hand_index == nil) or (GameRules:GetGameModeEntity().game_status == 1 and target.hand_index == nil and (GameRules:GetGameModeEntity().prepare_timer > 33 + extra_time or GameRules:GetGameModeEntity().prepare_timer < 2)) then
 	--（略）
 end
